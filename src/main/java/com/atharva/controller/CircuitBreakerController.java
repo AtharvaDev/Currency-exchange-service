22e4b0b322ac44d4587c680ca235fda90e3ee1c0
Atharva Deshmukh <atharva1525@gmail.com>
2021-11-19 23:13:52 +0530

    @CircuitBreaker mapping added

--- src/main/java/com/atharva/controller/CircuitBreakerController.java
+++ src/main/java/com/atharva/controller/CircuitBreakerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -17,6 +18,15 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	@Retry(name = "someName", fallbackMethod = "anyMethodCall")
 	public String sampleApi() {
+		logger.warn("sample api call received");
+//		here a false call is made to get an idea of what happens when api is down
+		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/someTest", String.class);
+		return entity.getBody();
+	}
+	
+	@GetMapping("/sample-api2")
+	@CircuitBreaker(name = "anyNameCB", fallbackMethod = "anyMethodCall")
+	public String sampleApi2() {
 		logger.warn("sample api call received");
 //		here a false call is made to get an idea of what happens when api is down
 		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/someTest", String.class);
