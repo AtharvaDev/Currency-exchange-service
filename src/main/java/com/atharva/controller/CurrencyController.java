ecfc0f14cb06c375f398a0d3ff6569353182648a
Atharva Deshmukh <atharva1525@gmail.com>
2021-11-19 15:52:36 +0530

    netflix-eureka-client added

--- src/main/java/com/atharva/controller/CurrencyController.java
+++ src/main/java/com/atharva/controller/CurrencyController.java
@@ -1,7 +1,5 @@
 package com.atharva.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +21,7 @@ public class CurrencyController {
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getvalue(@PathVariable String from, @PathVariable String to) {
 //		CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
-		
+
 		CurrencyExchange currencyExchange = currencyExchangeRepo.findByFromAndTo(from, to);
 		if (currencyExchange ==  null) {
 			throw new RuntimeException("no data found");
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 insert into currency_exchange
 (id, curreny_from, curreny_to, conversion_multiple, environment)
-values(1001, 'USD', 'INR', 10, '');
+values(1001, 'USD', 'INR', 74.21,'');
 
 insert into currency_exchange
 (id, curreny_from, curreny_to, conversion_multiple, environment)
-values(1002, 'EUR', 'INR', 20, '');
+values(1002, 'EUR', 'INR', 84.05, '');
 
 insert into currency_exchange
 (id, curreny_from, curreny_to, conversion_multiple, environment)
-values(1003, 'CAD', 'INR', 30, '');
+values(1003, 'CAD', 'INR', 58.82, '');
 
